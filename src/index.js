61eaa446f9d2a461921e2e237b713cdecadd9fd6
nitinsonkar3 <nitinsonkar2004ss@gmail.com>
2026-07-15 08:22:30 +0530

    update indexjs

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import connectDB from "./config/database.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log( ` Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!" , err);
+    
+})
